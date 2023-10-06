563618ebd0b798e75ab1ca5790b5e27486d714ec
Daniel <danieldcc.colares@gmail.com>
2023-10-06 16:31:08 -0300

    feat: add function to save user id to cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,8 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -32,7 +34,19 @@ export default function Register() {
   })
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err.response?.data?.message) {
+        alert(err?.response?.data.message)
+        return
+      }
+
+      console.error(err)
+    }
   }
 
   useEffect(() => {
