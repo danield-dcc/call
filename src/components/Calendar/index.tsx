84d5bd2051e9184967f1d1d3952dc5dc5cd51df9
Daniel <danieldcc.colares@gmail.com>
2023-10-11 17:06:45 -0300

    feat: initial schedule page

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,60 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarAction,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { getWeekDays } from '@/utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekdays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Outubro <span>2023</span>
+        </CalendarTitle>
+
+        <CalendarAction>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarAction>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekdays.map((weekDay) => {
+              return <th key={weekDay}>{weekDay}.</th>
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
