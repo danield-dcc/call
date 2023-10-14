4a8180b82ed1e31a10f74ad27c9d074e92bf3f96
Daniel <danieldcc.colares@gmail.com>
2023-10-14 11:40:06 -0300

    feat: loading and showing available schedules

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,88 @@
+/* eslint-disable camelcase */
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exists.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+
+  const isPasteDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPasteDate) {
+    // nada disponível neste dia
+    return res.json({ possibleTimes: [], availability: [] })
+  }
+
+  // fazer um cruzamento de dado entre o timeInterval(que o usuário selecionou) com os schedules
+  // passar pelo userTimeInterval, validando  se existem schedules suficientes para cobrir todos os
+  // intervalos que o usuário tem, se não quais são os horários que nãi há schedules ativos
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({ possibleTimes: [], availability: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+
+  // dados forma salvos em minutos, converter para horas(1h)
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i
+    },
+  )
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+    select: {
+      date: true,
+    },
+  })
+
+  // percorrer todos os horários possíveis, mantendo apenas quando não existe agendamento naquele horário
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return res.json({ possibleTimes, availableTimes })
+}
