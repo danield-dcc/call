faf2803790b8752290313ea9f46093775155e3c8
Daniel <danieldcc.colares@gmail.com>
2023-10-14 11:54:28 -0300

    feat: adds react query to fetch user schedules

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,8 @@ import { globalStyles } from '@/styles/global'
 import { Roboto } from 'next/font/google'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 const roboto = Roboto({
   weight: ['400', '500', '700'],
@@ -18,10 +20,12 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <main className={roboto.className}>
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { api } from '@/lib/axios'
 import { Calendar } from '@/components/Calendar'
 import {
@@ -10,6 +10,7 @@ import {
 } from './styles'
 import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
 
 interface Availability {
   possibleTimes: number[]
@@ -19,7 +20,6 @@ interface Availability {
 export function CalendarStep() {
   const router = useRouter()
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState<Availability | null>(null)
 
   const username = String(router?.query?.username)
   const isDateSelected = !!selectedDate
@@ -29,21 +29,23 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format('DD[ de ] MMMM')
     : null
 
-  useEffect(() => {
-    if (!selectedDate) {
-      return
-    }
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
 
-    api
-      .get(`/users/${username}/availability`, {
-        params: {
-          date: dayjs(selectedDate).format('YYYY-MM-DD'),
-        },
+  const { data: availability } = useQuery<Availability>(
+    ['availability', selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
+        params: { date: selectedDateWithoutTime },
       })
-      .then((response) => {
-        setAvailability(response.data)
-      })
-  }, [selectedDate, username])
+
+      return response.data
+    },
+    {
+      enabled: !!selectedDate,
+    },
+  )
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
